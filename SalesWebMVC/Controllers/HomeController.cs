d0d20bdc573f74a0afd983236038cea23932afcf
Carlos <carloshpsantos1996@gmail.com>
2022-09-22 16:09:17 -0300

    First controller and Razor pages tests

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -23,6 +23,13 @@ namespace SalesWebMVC.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Sales Web MVC App.";
+            ViewData["Name"] = "Carlos Henrique";
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
