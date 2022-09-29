72e501f45e376907e7096df6e35d0f50e10853a2
Carlos <carloshpsantos1996@gmail.com>
2022-09-29 16:09:25 -0300

    SellerFormViewModel and Department select component

--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -6,8 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<SalesWebMVCContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWebMVCContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMVCContext' not found.")));
 
-//builder.Services.AddScoped<SeedingService>();
 builder.Services.AddScoped<SellerService>();
+builder.Services.AddScoped<DepartmentService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
